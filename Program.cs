d54c58ff27d3f46655e285d044e36b4e2a32c334
Quadrate Tech Solutions <info@quadrate.lk>
2021-05-26 11:28:24 +0530

    var orderNumber = new StringBuilder();
                for (int x = 0; x < 100; x++)
                {
                    orderNumber.Append(x.ToString() + ", ");
                }
    
                Console.WriteLine(orderNumber);
                Console.ReadLine();

--- Program.cs
+++ Program.cs
@@ -8,6 +8,14 @@ namespace Reshaprer
         static void Main(string[] args)
         {
             var orderNumber = new StringBuilder();
+
+            for (int x = 0; x < 100; x++)
+            {
+                orderNumber.Append(x.ToString() + ", ");
+            }
+
+            Console.WriteLine(orderNumber);
+            Console.ReadLine();
         }
     }
 }
