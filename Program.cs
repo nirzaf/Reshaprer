903fedf35cb815652fc0b5acc3f41bd8b089a232
Quadrate Tech Solutions <info@quadrate.lk>
2021-05-26 11:25:45 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text;
+
+namespace Reshaprer
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            var orderNumber = new StringBuilder();
+        }
+    }
+}
